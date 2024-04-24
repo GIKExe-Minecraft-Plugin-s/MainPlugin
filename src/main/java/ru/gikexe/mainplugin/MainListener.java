956b34174eb20b4dd8120f7a4a67ba117b20a0d1
GIKExe <72767917+GIKExe@users.noreply.github.com>
2024-04-24 07:20:24 +0300

    Сбор урожая (событие)

--- src/main/java/ru/gikexe/mainplugin/MainListener.java
+++ src/main/java/ru/gikexe/mainplugin/MainListener.java
@@ -1,9 +1,7 @@
 package ru.gikexe.mainplugin;
 
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.Sound;
-import org.bukkit.World;
+import net.kyori.adventure.text.Component;
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.Ageable;
 import org.bukkit.entity.Player;
@@ -11,15 +9,26 @@ import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
 import org.bukkit.util.Vector;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 import java.util.List;
 
+import static net.kyori.adventure.text.format.NamedTextColor.*;
+import static net.kyori.adventure.text.format.TextDecoration.ITALIC;
+
 public class MainListener implements Listener {
 	MainPlugin plugin;
+	@Nullable Objective logic;
+	@Nullable Objective decor;
+	Integer cerf_x = 50;
 	List<Material> harvested_crops = List.of(
 					Material.WHEAT,
 					Material.CARROTS,
@@ -31,6 +40,13 @@ public class MainListener implements Listener {
 
 	public MainListener(MainPlugin plugin) {
 		this.plugin = plugin;
+
+    // временный код
+		logic = Bukkit.getScoreboardManager().getMainScoreboard().getObjective("farmer_event_logic");
+		if (logic == null) plugin.getLogger().warning("на сервере нет задачи farmer_event_logic!");
+
+		decor = Bukkit.getScoreboardManager().getMainScoreboard().getObjective("farmer_event");
+		if (decor == null) plugin.getLogger().warning("на сервере нет задачи farmer_event!");
 	}
 
 	@EventHandler
@@ -60,23 +76,32 @@ public class MainListener implements Listener {
 		for (ItemStack toSpawn : drop) world.dropItem(loc, toSpawn).setVelocity(new Vector(0, 0, 0));
 		event.setUseInteractedBlock(Event.Result.DENY);
 		event.setUseItemInHand(Event.Result.DENY);
-	}
 
-//	@EventHandler
-//	public void on(BlockBreakEvent event) {
-//		if (event.isCancelled()) return;
-//		Player player = event.getPlayer();
-//		Block block = event.getBlock();
-//		ItemStack item = player.getInventory().getItemInMainHand();
-//
-//		if (
-//			!block.getDrops(item, player).isEmpty() &&
-//			item.getType() != Material.TRIDENT &&
-//			item.containsEnchantment(Enchantment.LOYALTY) &&
-//			player.isSneaking()
-//		) {
-//			Material _type = block.getType();
-//			Integer _counter = 0;
-//		}
-//	}
+		// временный код
+		if (logic == null) return;
+		Score score = logic.getScore(player);
+		if (score.getScore() < 0) return;
+		score.setScore(score.getScore()+1);
+		if (decor != null) {
+			decor.getScore(player).setScore(decor.getScore(player).getScore()+1);
+		}
+		if (score.getScore() >= cerf_x) {
+			score.setScore(score.getScore()-cerf_x);
+			ItemStack cerf = new ItemStack(Material.PAPER);
+			ItemMeta im = cerf.getItemMeta();
+			im.displayName(Component.text("Сертификат", DARK_PURPLE).decoration(ITALIC, false));
+			im.lore(List.of(Component.text(cerf_x+" очков", GREEN).decoration(ITALIC, false),
+							Component.text("принадлежит игроку "+player.getName(), GRAY).decoration(ITALIC, false)));
+			cerf.setItemMeta(im);
+			player.getInventory().addItem(cerf);
+		}
+	}
+	
+	// временный код
+	@EventHandler
+	public void on(PlayerDeathEvent event) {
+		Player player = event.getPlayer();
+		if (logic == null) return;
+		logic.getScore(player).setScore(0);
+	}
 }
